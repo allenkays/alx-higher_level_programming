758d921f61189e6290a4e673b0f36096942376c8
allenkays <allenkays@tutanota.com>
2022-11-29 13:34:29 -0500

    lowercase check

---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+islower - function to check for lowercase in string
+@ c: string to check
+'''
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
