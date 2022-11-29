7d88a0330a81a751251bd1003a91b83e30ed9b2b
allenkays <allenkays@tutanota.com>
2022-11-29 15:28:13 -0500

    print alphabet in reverse

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''
+lower_uppercase- converts given string to uppercase
+
+'''
+
+
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
