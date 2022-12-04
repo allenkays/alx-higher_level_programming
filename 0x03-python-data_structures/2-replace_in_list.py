175084ecb151ccf26733c51b27f30bfdb62c93d3
allenkays <allenkays@tutanota.com>
2022-12-04 16:08:50 -0500

    edit comment

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@
 
 
 def replace_in_list(my_list, idx, element):
-    """Replace  ele of lst at a specified position."""
+    """Replace item of list at a specified index."""
     
     if idx >= 0 and idx < len(my_list):
         my_list[idx] = element
