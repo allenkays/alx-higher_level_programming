0b0f68f1228d5f677256d9c760b9581e7b09c5e5
allenkays <allenkays@tutanota.com>
2022-12-04 16:28:32 -0500

    edit list copy

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,5 @@
 
 def print_reversed_list_integer(my_list=[]):
     """Print all items of a list in reverse order."""
-    return my_list[::-1]
+
+    return my_list[:-1]
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,6 +8,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
-    cpy = [0:]
+    cpy = [i for i in my_list]
     cpy[idx] = element
     return (cpy)
