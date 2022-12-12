99723acd9ad592ea076fabfef15bee88f44fb635
allenkays <allenkays@tutanota.com>
2022-12-12 14:27:37 -0500

    calculate weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    """Return weighted average of int in a list of tuples."""
+    if my_list != list(my_list) or len(my_list) == 0:
+        return (0)
+
+    average = 0
+    size = 0
+    for x in my_list:
+        average += (x[0] * x[1])
+        size += x[1]
+    return (average / size)
