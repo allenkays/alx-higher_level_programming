1c72f66751e0fabeed556c6d1c3869837e5957fd
allenkays <allenkays@tutanota.com>
2022-12-19 14:16:26 -0500

    fix first line error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,7 @@
-
 #!/usr/bin/python3
-# 3-safe_print_division.py
 
 
 def safe_print_division(a, b):
-    """return a/b."""
     try:
         res = a / b
     except (TypeError, ZeroDivisionError):
