2e6bfe9128ae148252bf3669b78b50d9970b4f6f
allenkays <allenkays@tutanota.com>
2022-12-20 16:05:26 -0500

    Add commemts

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,19 +4,37 @@
 
 
 class Square:
+    """Represents a square
+    Attributes:
+        __size (int): len of side of a square
+    """
 
     def __init__(self, size=0):
-        """defining a square class"""
+        """defining a square class
+        Args:
+            size(int): len of side of square
+        Returns:
+            None
+        """
         self.__size = size
 
     @property
     def size(self):
-        """getter"""
+        """gets the size of square
+        Returns:
+            size of square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """setas the value to params"""
+        """set as the value to params
+        Args:
+            value (int): len of a square
+        returns:
+            None
+        """
+
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
@@ -26,5 +44,8 @@ class Square:
                 self.__size = value
 
     def area(self):
-        """Calculates the area of a square and returns result"""
-        return (self.__size ** 2)
+        """Calculates the area of a square and returns result
+        Args:
+            value (int): size of squares
+        """
+        return (self.__size) ** 2
