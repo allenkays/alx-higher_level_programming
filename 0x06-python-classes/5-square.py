fad49b18730e17cf66de3ba62ed7f559f6969af1
allenkays <allenkays@tutanota.com>
2022-12-20 16:11:12 -0500

    add comments and method description

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,18 +1,40 @@
 #!/usr/bin/python3
+# 4-square.py
+"""setter and getter option creation"""
 
 
 class Square:
+    """Represents a square
+    Attributes:
+        __size (int): len of side of a square
+    """
 
     def __init__(self, size=0):
-        """defining a square class"""
+        """defining a square class
+        Args:
+            size(int): len of side of square
+        Returns:
+            None
+        """
         self.__size = size
 
     @property
     def size(self):
+        """gets the size of square
+        Returns:
+            size of square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """set as the value to params
+        Args:
+            value (int): len of a square
+        returns:
+            None
+        """
+
         if type(value) is not int:
             raise TypeError("size must be an integer")
         else:
@@ -22,8 +44,11 @@ class Square:
                 self.__size = value
 
     def area(self):
-        """Calculates the area of a square and returns result"""
-        return (self.__size ** 2)
+        """Calculates the area of a square and returns result
+        Args:
+            value (int): size of squares
+        """
+        return (self.__size) ** 2
 
     def my_print(self):
         """prints the square with '#'
