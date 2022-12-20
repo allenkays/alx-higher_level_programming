a3a61bbe8cfc81896837346b5ce373951dd3f81f
allenkays <allenkays@tutanota.com>
2022-12-20 15:18:41 -0500

    pycodestyle edits

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size=0):
+        """defining a square class"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+
+    def area(self):
+        """Calculates the area of a square and returns result"""
+        return (self.__size ** 2)
