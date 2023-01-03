9f209033b6954e26eb30a95766ab884cbb2e4d42
allenkays <allenkays@tutanota.com>
2023-01-03 15:34:54 -0500

    test name print function

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# function that prints out the fullnames of a person
+
+def say_my_name(first_name, last_name=""):
+    """
+        Args:
+            first_name(str): firstname
+            last_name(str): lastname
+        Raises:
+            TypeError: if firstname and lastname are not strings
+    """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
