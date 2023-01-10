6371e2e0058a03f0e090bcb03fc8cee5bcada357
allenkays <allenkays@tutanota.com>
2023-01-10 15:12:29 -0500

    Pascal triangle implementation

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -13,4 +13,4 @@ def pascal_triangle(n=4500):
             pascal[i][j+1] = pascal[i-1][j] + pascal[i-1][j+1]
             pascal[i][i-j-1] = pascal[i-1][j] + pascal[i-1][j+1]
 
-    return pascal 
+    return pascal
