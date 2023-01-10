496465073e8d08ed1e53e4472dc13ec46f17729e
allenkays <allenkays@tutanota.com>
2023-01-09 21:48:15 -0500

    Add a public attribute to base class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Contains the class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """Add an attribute to class"""
+    def area(self):
+        """raises an exception when called"""
+        raise Exception("area() is not implemented")
