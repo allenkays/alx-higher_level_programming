b7045c500a00181298777f6d5f05e3ee20b04652
allenkays <allenkays@tutanota.com>
2023-02-15 22:46:11 -0500

    Create database and table

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- Creaates the database hbtn_0d_usa and the table cities
+-- cities description:
+	-- id INT unique, auto generated, can’t be null and is a primary key
+    	-- state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
+    	-- name VARCHAR(256) can’t be null
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+	PRIMARY KEY(id),
+	id	INT	NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256) NOT NULL
+	
+);
