54fe1978cb2249bca3f443ae56d9ba34b8d9cef7
allenkays <allenkays@tutanota.com>
2023-03-21 06:51:29 -0700

    Remove try statement

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -3,24 +3,26 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
+    """
     if len(sys.argv) != 4:
         print('Usage: {} <username> <password> <database>'.format(sys.argv[0]))
         sys.exit(1)
+    """
 
     username, password, database = sys.argv[1:]
 
     # Connect to MySQL server
-    try:
-        db = MySQLdb.connect(
-                user=username,
-                passwd=password,
-                db=database,
-                host='localhost',
-                port=3306
-                )
-    except Exception as e:
-        print('Error connecting to MySQL server:', e)
-        sys.exit(1)
+    # try:
+    db = MySQLdb.connect(
+            user=username,
+            passwd=password,
+            db=database,
+            host='localhost',
+            port=3306
+            )
+    #except Exception as e:
+    print('Error connecting to MySQL server:', e)
+    sys.exit(1)
 
     # Execute query
     cursor = db.cursor()
