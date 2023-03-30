8b925892a0bf3f24878e2a91a75022b8c38d35cd
allenkays <allenkays@tutanota.com>
2023-03-30 14:47:05 -0400

    Add quotes to variable

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# Print the size in bytes of the http header response` of a given url
-curl -s $1 | wc -c
+# Print size in bytes of the http header response` of a given url
+curl -s "$1" | wc -c
