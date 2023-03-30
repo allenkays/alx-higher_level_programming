84c4a7c447f9d1ad0c08ef80dca367b17b567e4e
allenkays <allenkays@tutanota.com>
2023-03-30 14:41:33 -0400

    Remove the -w option

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-# Print the bytesize of the http header of a given url
-curl -s -w -o /dev/null $1 | wc -c
+# Print the size in bytes of the http header response` of a given url
+curl -s $1 | wc -c
