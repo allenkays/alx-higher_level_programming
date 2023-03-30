569ca0d621d527cd397bbf9af8ba1d65eaaf6dcb
allenkays <allenkays@tutanota.com>
2023-03-30 17:26:17 -0400

    Add -L option

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This script sends a GET request to a given url and displays its body on success
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+#This script sends a GET request to a given URL and displays its body on success
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -sL "$1"
