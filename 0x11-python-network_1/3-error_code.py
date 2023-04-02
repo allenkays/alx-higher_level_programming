66fad520c7a7e9c96f4d1fd34a4fc9b99cd9d92c
allenkays <allenkays@tutanota.com>
2023-04-02 08:20:24 -0400

    Capturing HTTP Error codes in python

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -4,9 +4,10 @@ This script takes url input and sends a request to it
 displaying the body of the response decoded as utf-8
 """
 
-import sys
-import urlib.requests
+from sys import argv
+import urllib.request
 import urllib.error
+
 
 if __name__ == "__main__":
     """
@@ -16,8 +17,8 @@ if __name__ == "__main__":
 
     url = argv[1]
     try:
-        with urllib.requests.urlopen(url) as response:
+        with urllib.request.urlopen(url) as response:
             body = respose.read
             print(body.decode('utf-8'))
-    Exception:
-        print(f"{urllib.error.HTTPError(url).code}")
+    except urllib.error.HTTPError as e:
+        print(f"{e.code}")
