f1b49cc4f55f764b7f35a595d83ae3688d20a462
allenkays <allenkays@tutanota.com>
2023-04-02 07:54:30 -0400

    Posting with given creds

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,13 +8,13 @@ from sys import argv
 import urllib.request
 import urllib.parse
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """
     This should display the body of the response decoded as utf-8
     """
 
     r = urllib.parse.urlencode({'email': argv[2]}).encode('utf-8')
-    data = urllib.request.urlopen(argv[1], email=r)
+    data = urllib.request.urlopen(argv[1], data=r)
     with data as response:
         body = response.read()
         print(body.decode('utf-8'))
