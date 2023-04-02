3cae7f23acc5422448866fd6b25f2f0e45c847d1
allenkays <allenkays@tutanota.com>
2023-04-02 09:56:21 -0400

    Revert format style and add new print statements

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -12,5 +12,6 @@ if __name__ == "__main__":
     """
 
     url = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:\n\t - type: {}\n\t - content: {}"
-          .format(type(url.text), url.text))
+    print("Body response:")
+    print("\t- type: {}".format(type(url.text)))
+    print("\t- content: {}".format(url.text))
