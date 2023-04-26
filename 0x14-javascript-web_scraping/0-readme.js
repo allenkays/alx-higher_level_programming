b27b3effc297b603618354b3170f5c3cf2646a4d
allenkays <allenkays@tutanota.com>
2023-04-26 16:22:15 -0400

    Edit function call

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf-8', (err, content) => {
+fs.readFile(filePath, 'utf-8', function (err, content) => {
   console.log(error || content);
 });
