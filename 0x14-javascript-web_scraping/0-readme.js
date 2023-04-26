44d929ef7b436f0db0d77c3aa884ca6665875746
allenkays <allenkays@tutanota.com>
2023-04-26 16:20:15 -0400

    remove tab space

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 fs.readFile(filePath, 'utf-8', (err, content) => {
-	console.log(error || content);
+  console.log(error || content);
 });
